11bf430ec44758bf758dfa419ab3e2a168bc6c5c
PaingSuLt <pkhazel72@gmail.com>
2026-09-21 21:33:16 +0630

    add 2 new photos

--- src/photo.js
+++ src/photo.js
@@ -2,26 +2,33 @@ import photo1 from "./assets/firstConnection.jpg";
 import photo2 from "./assets/photo2.jpg";
 import photo3 from "./assets/photo3.jpg";
 import photo4 from "./assets/photo4.jpg";
+import photo5 from "./assets/photo5.png";
 import songFile from "./assets/song.mp3";
 
 export const photos = [
   {
     src: photo1,
-    caption: "Look at that guy stares 💕",
+    caption: "To the most amazing girl I know",
   },
   {
     src: photo2,
-    caption: "Aneki gazing her bright future from the toilet window",
+    caption:
+      "May your days ahead shine as bright as the sky you looked out at from that window",
   },
   {
     src: photo3,
-    caption: " မမရဲ့ ပုံရိုက်ရင် အသုံးများဆုံး pose 😂",
+    caption: " Always the sleepy pose",
   },
   {
     src: photo4,
-    caption: "Wishing you a future filled with endless happiness and joy",
+    caption: "Wishing you a future filled with endless happiness and joy🥰",
+  },
+  {
+    src: photo5,
+    caption:
+      "Remember aneki. Age is just a number. In your case, a really, really high one🤣. Have the best birthday!❤️❤️",
   },
 ];
 
 export const songSrc = songFile;
-export const slideInterval = 5000;
+export const slideInterval = 6500;
